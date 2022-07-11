580c292e5e94e0ca44cfa3064b59f7b86dd0b295
Ariadna <ariadnajiro@gmail.com>
2022-07-11 23:19:39 +0200

    portfolio actualizada amb map

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,42 +1,40 @@
 import { Navbar, Nav, Container, NavLink, Row, Col } from "react-bootstrap";
 
 function Portfolio() {
+
+  const proyectos = [
+    { id: 1, url: "https://unsplash.it/500/700" },
+    { id: 2, url: "https://unsplash.it/800/400" },
+    { id: 3, url: "https://unsplash.it/400/800" },
+    { id: 4, url: "https://unsplash.it/800/600" },
+    { id: 5, url: "https://unsplash.it/500/400" },
+    { id: 6, url: "https://unsplash.it/500/605" },
+    { id: 7, url: "https://unsplash.it/400/300" },
+    { id: 8, url: "https://unsplash.it/500/600" },
+  ];
+
+  let fotografias = proyectos.map((foto) => {
+    return (
+      <>
+        <div class="content flow">
+          <img src={foto.url} alt=""/>
+        </div>
+
+      </>
+    );
+  });
+
   return (
     <>
       <Row id="portfoli">
         <div id="cont_portfolio">
           <div className="apartado">Portfolio</div>
-
-          <div class="grid">
-            <div class="content flow">
-              <img src="https://unsplash.it/500/700" alt="" />
-            </div>
-            <div class="content flow">
-              <img src="https://unsplash.it/800/400" alt="" />
-            </div>
-            <div class="content flow">
-              <img src="https://unsplash.it/400/800" alt="" />
-            </div>
-            <div class="content flow">
-              <img src="https://unsplash.it/800/600" alt="" />
-            </div>
-            <div class="content flow">
-              <img src="https://unsplash.it/500/400" alt="" />
-            </div>
-            <div class="content flow">
-              <img src="https://unsplash.it/500/600" alt="" />
-            </div>
-            <div class="content flow">
-              <img src="https://unsplash.it/400/300" alt="" />
-            </div>
-            <div class="content flow">
-              <img src="https://unsplash.it/500/600" alt="" />
-            </div>
-          </div>
+          <div class="grid">{fotografias}</div>
         </div>
       </Row>
     </>
   );
 }
+
 
 export default Portfolio;
